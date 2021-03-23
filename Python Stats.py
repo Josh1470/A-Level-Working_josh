be2da39eb11e394252c7a9a74d63adc4abd0cbed
jossol07 <jossol07@highgateschool.org.uk>
2021-03-23 13:07:49 +0000

    Josh's version

--- Python Stats.py
+++ Python Stats.py
@@ -10,3 +10,6 @@ x_with_nan = [8.0, 1, 2.5, math.nan, 4, 28.0]
 y, y_with_nan = np.array(x), np.array(x_with_nan)
 z, z_with_nan = pd.Series(x), pd.Series(x_with_nan)
 w = [0.1, 0.2, 0.3, 0.25, 0.15]
+
+for i in range(10):
+    print (i)
